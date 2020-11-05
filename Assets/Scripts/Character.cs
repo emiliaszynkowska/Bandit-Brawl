f4cbfff9a1a7899caa4a600a5ef6db368d50068f
wljd1g18 <wljd1g18@soton.ac.uk>
2020-11-05 22:46:39 +0000

    Added player movement, basic animations and collisions

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //constants
+    protected const float groundDistance = 0.5f;
+    protected const int doubleJumpsCount = 2;
+
+    //public variables
+    public float health;
+    public float moveSpeed;
+    public float jumpSpeed;
+    public float jumpCooldown;
+    public Animator animator;
+
+    //protected variables
+    protected float lastJumpTime;
+    protected SpriteRenderer sprite;
+    protected bool isGrounded = true;
+    [SerializeField] protected LayerMask collisionMask;
+    protected int jumpsRemaining;
+
+   /* private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == ("Foreground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == ("Foreground"))
+        {
+            isGrounded = false;
+        }
+    }*/
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Inherits from Character class
+public class Player : Character
+{
+    Rigidbody2D body;
+    public Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpsRemaining = doubleJumpsCount;
+        body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroundCheck();
+        PlayerJump();
+    }
+
+    void GroundCheck()
+    {
+        if (Time.time < lastJumpTime + jumpCooldown) return;
+        isGrounded = Physics2D.Raycast(body.position, Vector2.down, groundDistance ,collisionMask);
+        Debug.DrawRay(body.position, Vector3.down* groundDistance, Color.green, 0);
+        Debug.Log(isGrounded);
+        if (isGrounded)
+        {
+            AnimateGrounded();
+            jumpsRemaining = doubleJumpsCount;
+        }
+        else
+        {
+            AnimateJump();
+        }
+    }
+
+    private void AnimateGrounded()
+    {
+        animator.SetBool("Grounded", true);
+        animator.SetFloat("AirSpeed", 0);
+    }
+
+    private void AnimateJump()
+    {
+        animator.SetBool("Grounded", false);
+        animator.SetBool("Jump", true);
+        animator.SetFloat("AirSpeed", body.velocity.magnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        CameraFollow();
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        float horizontalmove = Input.GetAxis("Horizontal");
+        float verticalmove = Input.GetAxis("Vertical");
+        if (horizontalmove > 0)
+        {
+            body.velocity = new Vector2(moveSpeed,body.velocity.y);
+            sprite.flipX = true;
+            animator.SetInteger("AnimState", 2);
+        }
+        if (horizontalmove < 0)
+        {
+            body.velocity = new Vector2(-moveSpeed, body.velocity.y);
+            sprite.flipX = false;
+            animator.SetInteger("AnimState", 2);
+        }
+        else if (horizontalmove == 0)
+        {
+            animator.SetInteger("AnimState", 0);
+        }
+        
+
+    }
+
+    void CameraFollow()
+    {
+        cam.transform.position = transform.position + Vector3.back;
+    }
+
+    void PlayerJump()
+    {
+        if (jumpsRemaining>0 && Input.GetButtonDown("Jump") && Time.time > lastJumpTime + jumpCooldown)
+        {
+            lastJumpTime = Time.time;
+            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+            jumpsRemaining--;
+        }
+    }
+}
